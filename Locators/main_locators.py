ae05e92740344a8fbac63f0e9e57addf12117bf0
natasharomancuk <natalka.shv@gmail.com>
2025-10-14 21:19:05 +0300

    1 commit

---
+++ Locators/main_locators.py
@@ -0,0 +1,111 @@
+import os
+from nuts_nuts_nuts.page1.base_page import WebPage
+from nuts_nuts_nuts.page1.elements import WebElement ,ManyWebElements
+
+
+class MainPage(WebPage):
+    def __init__(self, web_driver, url=''):
+        if not url:
+            url = os.getenv('MAIN_URL') or 'https://emall.by/'
+
+        super().__init__(web_driver, url)
+
+
+    logotip = WebElement(xpath='//div[@id="header__inner"]//*[@class="logo_logo__LbCcG"]')
+    search_input = WebElement(xpath='//div[@id="header__inner"]//input[@placeholder="Поиск товаров"]')
+    search_button = WebElement(xpath='//*[@aria-label="Начать поиск"]')
+    catalog_button = WebElement(xpath='//*[@class="catalog_button__yhaM7"]')
+
+    # авторизация
+    action_button_enter = WebElement(xpath='//div[@id="header__inner"]//span[contains(text(),"Войти")]')
+    action_button_orders = WebElement(xpath='//div[@id="header__inner"]//span[contains(text(),"Заказы")]')
+    action_button_favorites = WebElement(xpath='//div[@id="header__inner"]//span[contains(text(),"Избранное")]')
+    action_button_basket = WebElement(xpath='//a[@href="https://emall.by/cart"]')
+
+    # разделы
+    actions_button = WebElement(xpath='//div[@id="header__inner"]//a[contains(text(),"Акции")]')
+    lucky_goods_button = WebElement(xpath='//div[@id="header__inner"]//a[contains(text(),"Товары-везунчики")]')
+    promocodes_button = WebElement(xpath='//div[@id="header__inner"]//a[contains(text(),"Промокоды")]')
+    new_button = WebElement(xpath='//div[@id="header__inner"]//a[contains(text(),"Новинки")]')
+    urgent_goods_button = WebElement(xpath='//div[@id="header__inner"]//a[contains(text(),"Срочный товар!")]')
+    take_more_button = WebElement(xpath='//div[@id="header__inner"]//a[contains(text(),"Упаковкой выгоднее")]')
+    naushniki_besprovodnye_button = WebElement(xpath='//div[@id="header__inner"]//a[contains(text(),"Наушники беспроводные")]')
+    school_button = WebElement(xpath='//div[@id="header__inner"]//a[contains(text(),"Школа")]')
+    detskaya_stirka_button = WebElement(xpath='//div[@id="header__inner"]//a[contains(text(),"Детская стирка")]')
+
+    # верхнее меню
+    adress_text = WebElement(xpath='//div[@id="header__inner"]//span[contains(text(),"Адрес доставки")]')
+    ykazat_adress_button = WebElement(xpath='//div[@id="header__inner"]//button[contains(text(),"Указать")]')
+    payment_delivery_button = WebElement(xpath='//div[@id="header__inner"]//a[contains(text(),"Доставка и оплата")]')
+    oplata_chastyami_button = WebElement(xpath='//div[@id="header__inner"]//a[contains(text(),"Оплата частями")]')
+    seller_emall_button = WebElement(xpath='//div[@id="header__inner"]//a[contains(text(),"Продавайте на emall")]')
+    business_emall_button = WebElement(xpath='//div[@id="header__inner"]//a[contains(text(),"Emall для юр. лиц")]')
+    edostavka_button = WebElement(xpath='//div[@id="header__inner"]//a[contains(text(),"Edostavka")]')
+
+   # футер
+
+    compania_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Компания")]')
+    servise_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"О сервисе")]')
+    contacty_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Контакты")]')
+    news_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Новости")]')
+    vacansii_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Вакансии")]')
+    sotrydnichestvo_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Сотрудничество")]')
+    dostavca_dlya_urlic_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Доставка для юр.лиц")]')
+    postavsikam_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Поставщикам")]')
+    reklamodatelam_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Рекламодателям")]')
+    stat_prodavcom_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Стать продавцом")]')
+    prodaza_avto_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Продажа автомобилей")]')
+    pokypatelam_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Покупателям")]')
+    polz_soglashenie_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Пользовательское соглашение")]')
+    vopros_otvet_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Вопрос-ответ")]')
+    dostavka_oplata_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Доставка и оплата")]')
+    katalog_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Каталог")]')
+    tovari_ot_emall_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Товары от магазина Emall")]')
+    punkti_vidachi_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Пункты выдачи")]')
+    soglashenie_o_creditax_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Соглашение о кредитных ресурсах")]')
+    nashi_druzia_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Наши друзья")]')
+    edostavca_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Едоставка")]')
+    evropochta_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Европочта")]')
+    evroopt_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Евроопт")]')
+    xit_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Хит!")]')
+    groshik_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Грошык")]')
+    my_socsetyax_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Мы в соц. сетях")]')
+    nashi_prilozenia_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Наши приложения")]')
+    sviazatsa_snamy_button = WebElement(xpath='//footer[@id="footer"]//*[contains(text(),"Связаться с нами")]')
+    
+    cookies_button = WebElement(xpath='//button[@type="button"]//*[contains(text(),"Принять")]')
+    close_button = WebElement(xpath='//button[@type="button"]//*[contains(text(),"Закрыть")]')
+
+    brands_button = ManyWebElements(xpath='//a[@class="brands_link__3NOhi"]')
+
+    all_products = ManyWebElements(xpath='//div[@class="adult-wrapper_adult__yIhdE vertical_product__hiLyN products_product__DBmFj"]')
+    search_results = WebElement(xpath='//span[@class="search_heading_query__Ci6uB"]')
+
+    vkorzine_btn = ManyWebElements(xpath='//div[@class="vertical_actions__YjIyP"]')
+    v_korzine_dtn_1 = WebElement(xpath='(//div[@class="adult-wrapper_adult__yIhdE vertical_product__hiLyN products_product__DBmFj"]//span[contains(text(),"В корзину")])[1]')
+    korzina_product_name = WebElement(xpath='//a[@class="product_name__4NbXl"]')
+    main_product_name = WebElement(xpath='(//a[@class="vertical_title__FM_Ud"])[1]')
+    korzina_product_counter = WebElement(xpath='//div[@class="cart_prices__value__3mhYj"]//*[contains(text(),"товар")]')
+    product_counter = WebElement(xpath='//div[@class="product_counter__nqFWd"]//*[@aria-label="Количество"]')
+    korzina_product_price= WebElement(xpath='//div[@class="cart_prices__total__Gtyvn"]//*[contains(text(),"р.")]')
+    main_product_price= WebElement(xpath='(//div[@data-nosnippet="true"]//span[@class="price_main__ZI_hw price_main_red__nTPA_"])[1]')
+    main_skidka= WebElement(xpath='(//div[@bis_skin_checked="1"]//span[@class="tag_tag__1KrVp tag_tag_size_small__qndcB tag_tag_reverse__qv92T"])[1]')
+    bage_korzina_count= WebElement(xpath='//span[@class="badge-animation_badge__nBsm5 medium"]')
+    main_izbrannoe= WebElement(xpath='(//*[@aria-label="Добавить в избранное"])[1]')
+    main_img= WebElement(xpath='(//*[@class="product-webp-png-image_image__TCZIc card-image_image__eENi8"])[1]')
+    main_name= WebElement(xpath='(//a[@class="vertical_title__FM_Ud"])[1]')
+    favorite_name= WebElement(xpath='//a[@style="-webkit-line-clamp: 3;"]')
+    # brands= WebElement(xpath='(//*[@target="_self"])[27]//i')
+    brands= WebElement(xpath='//a[contains(@href, "brands")][@class="module_link__wJY1i"]')
+
+    brands_link=ManyWebElements(xpath='//a[@class="brands_brand_card__pOg7K"]')
+    katalog_prodycty=WebElement(xpath='(//*[@role="menu"]/*[1])')
+    made_in_belarus_toogle=WebElement(xpath='(//div[@class="toggle_toggle__WFDLb"])[1]')
+    toggle_checkbox=WebElement(xpath='(//input[@class="toggle_checkbox__jAMIn"])[1]')
+    # made_in_belarus_toogle=WebElement(xpath='//div[@class="toggle_toggle__WFDLb toggle_toggle_checked__EQf7Q"]')
+    # made_in_belarus_lables=ManyWebElements(xpath='//span[contains(text(), "Беларусь")]')
+    made_in_belarus_product=WebElement(xpath='(//div[@class="card-image_container__ag0cA"])[1]')
+    strana_product=WebElement(xpath='//span[@class="preview_full__value__XTBh6"]')
+    first_slide=WebElement(xpath='(//div[@data-swiper-slide-index="0"])[1]')
+
+
---
+++ tests/UI/test_main.py
@@ -0,0 +1,391 @@
+import allure
+import time
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+from nuts_nuts_nuts.conftest import chrome_options
+from Locators.main_locators import MainPage
+from nuts_nuts_nuts.conftest import web_browser
+import pytest_check as check
+from nuts_nuts_nuts.page1.base_page import WebPage
+from nuts_nuts_nuts.page1.elements import WebElement
+from nuts_nuts_nuts.page1.elements import ManyWebElements
+
+@allure.feature("Главная страница")
+@allure.story("Хэдер, проверка элементов на отображение и кликабельность")
+@allure.severity(allure.severity_level.NORMAL)
+def test_header_elements_displayed(web_browser, chrome_options):
+    with allure.step('Запуск и настройка браузера'):
+        driver = MainPage(web_browser)
+        driver.cookies_button.click()
+        driver.close_button.click()
+
+    with allure.step('Подготовка тестовых данных'):
+        elements = [
+            ( driver.logotip,"логотип"),
+            ( driver.search_input,"строка поиска"),
+            ( driver.search_button,"иконка поиска"),
+            ( driver.catalog_button,"кнопка каталог"),
+            ( driver.action_button_enter,"войти"),
+            ( driver.action_button_orders,"заказы"),
+            ( driver.action_button_favorites,"избранное"),
+            ( driver.action_button_basket,"корзина"),
+            ( driver.actions_button,"акции"),
+            ( driver.lucky_goods_button,"товары-везунчики"),
+
+    ]
+
+
+    with allure.step('Проверка элемента'):
+        for element, text_element in elements:
+            with allure.step(f'Проверка элемента {text_element} на отображение'):
+                check.is_true(element.is_visible(), f'Элемента {text_element} нет визуально')
+
+            with allure.step(f'Проверка элемента {text_element} на кликабельность'):
+                check.is_true(element.is_clickable(), f'Элемента {text_element} не кликабелен')
+
+@allure.feature("Главная страница")
+@allure.story("Хэдер, ввод запроса в поисковую строку и результат отображения")
+def test_search_product_valid_query(web_browser):
+    with allure.step('Запуск и настройка браузера'):
+        driver = MainPage(web_browser)
+        driver.cookies_button.click()
+        driver.close_button.click()
+
+    with allure.step('Вводим запрос и нажимаем Поиск'):
+        search_input = driver.search_input
+        search_input.send_keys("iPhone")
+        driver.search_button.click()
+        time.sleep(3)
+
+    with allure.step('Проверяем, что результаты поиска отображаются'):
+        check.is_true(driver.search_results.is_visible(), "Результаты поиска не отображаются")
+
+    with allure.step('Проверяем, что в результатах есть iPhone'):
+        results_text = driver.search_results.get_text().lower()
+        check.is_true("iphone" in results_text, f"'iPhone' не найден в результатах: {results_text}")
+
+@allure.feature("Главная страница")
+@allure.story("Хэдер, очистка строки поиска")
+def test_search_clear_field(web_browser):
+    with allure.step('Запуск и настройка браузера'):
+        driver = MainPage(web_browser)
+        driver.cookies_button.click()
+        driver.close_button.click()
+
+    with allure.step('Вводим запрос и очищаем строку'):
+        search_input = driver.search_input
+        search_input.send_keys("Молоко")
+        driver.search_button.click()
+
+    with allure.step('Проверяем, что поле поиска стало пустым'):
+        search_input.send_keys(Keys.CONTROL + "a")
+        search_input.send_keys(Keys.DELETE)
+        after_clear_value = search_input.get_attribute("value")
+        check.equal(after_clear_value, "", f"Ожидали пустое поле поиска, но получили '{after_clear_value}'")
+
+
+@allure.feature("Главная страница")
+@allure.story("Подсчет товаров на главной странице")
+def test_count_products(web_browser):
+    with allure.step('Запускаем и настройка браузер'):
+        driver = MainPage(web_browser)
+        driver.cookies_button.click()
+        driver.close_button.click()
+
+    with allure.step('Подсчет количества товаров'):
+        products = driver.all_products.count()
+        check.greater_equal(products, 10)  #от 10
+        check.less_equal(products, 99)      # до 99
+
+@allure.feature("Главная страница")
+@allure.story("Проверка отображения картинок у всех товаров")
+def test_check_product_images_visible(web_browser):
+    with allure.step('Запускаем браузер и открываем главную страницу'):
+        driver = MainPage(web_browser)
+        driver.cookies_button.click()
+        driver.close_button.click()
+
+    with allure.step('Получаем все товары на странице'):
+        products = driver.all_products.count()
+        check.greater_equal(products, 1, "На странице нет товаров для проверки картинок")
+
+    with allure.step('Проверяем, что у каждого товара картинка отображается'):
+        for i in range(products):
+            img_element = driver.all_products[i].find_element(By.TAG_NAME, "img")  # ищем <img> внутри каждого товара
+
+            img_src = img_element.get_attribute("src")
+            check.is_not_none(img_src, f"У товара №{i+1} отсутствует картинка (src=None)")
+            check.not_equal(img_src.strip(), "", f"У товара №{i+1} пустой src для картинки")  # проверяем, что атрибут src не пустой
+            check.is_true(img_element.is_displayed(), f"Картинка у товара №{i+1} не отображается на странице")  # проверяем, что картинка реально отображается
+
+@allure.feature('Главная страница')
+@allure.story('Кнопки "В корзине" кликабельны')
+def test_vkorzine(web_browser):
+    with allure.step('Запуск и настройка браузера'):
+        driver = MainPage(web_browser)
+        driver.cookies_button.click()
+        driver.close_button.click()
+
+    with allure.step("Скроллим страницу вниз"):
+        driver._web_driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    with allure.step("Проверяем кликабельность кнопок 'В корзине'"):
+        vkorzine_buttons = driver.vkorzine_btn.find()  # получаем список кнопок
+        if not vkorzine_buttons:
+            print("Кнопки 'В корзине' не найдены!")
+            assert False, "Кнопки 'В корзине' не найдены!"
+
+        wait = WebDriverWait(driver._web_driver, 10)
+
+        for i, btn in enumerate(vkorzine_buttons, start=1):
+            driver._web_driver.execute_script("arguments[0].scrollIntoView(true);", btn)   # Скроллим к кнопке
+
+            try:
+                wait.until(EC.element_to_be_clickable(btn))    # Ждём, пока кнопка станет кликабельной
+                print(f"Кнопка {i} кликабельна ")
+            except:
+                print(f"Кнопка {i} НЕ кликабельна ")
+
+
+@allure.feature('Главная страница')
+@allure.story('Добавление товара в корзину и проверка его отображения, количества и цены')
+def test_add_goods_and_check_cart(web_browser):
+    with allure.step('Запуск и настройка браузера'):
+        driver = MainPage(web_browser)
+        driver.cookies_button.click()
+        driver.close_button.click()
+
+    with allure.step('Добавляем товар в корзину'):
+        driver.v_korzine_dtn_1.click()
+        main_prise = driver.main_product_price.get_text()
+        main_prise = main_prise.split('\n')[0]
+        main_prise = main_prise.replace(' р.', '')
+        main_prise = main_prise.replace(',', '.')
+
+    with allure.step('Переходим в корзину'):
+        driver.action_button_basket.click()
+
+    with allure.step('Проверяем, что товар отображается в корзине'):
+        product_name_1 = driver.main_product_name.get_text()
+        product_name = driver.korzina_product_name.get_text()
+        assert product_name != "", f"Товар не появился в корзине"
+        assert product_name_1 == product_name, f"Названия товаров не совпадают"
+
+    with allure.step('Проверяем количество товара'):
+        counter = driver.product_counter.get_attribute('value')  # если input
+        bage = driver.bage_korzina_count.get_text()  # если текст внутри элемента
+
+        assert counter == bage, f'Ожидали количество{counter}, а получили количество {bage}'
+
+    with allure.step('Проверяем цену товара'):
+        price = driver.korzina_product_price.get_text()
+        price = price.replace(' р.', '')
+        price = price.replace(',', '.')
+        print((price))
+        print((main_prise))
+        assert float(price) > 0
+        assert price == main_prise
+
+
+@allure.feature('Главная страница')
+@allure.story('Добавление товара в "Избранное" и его отображение в "Избранное"')
+def test_add_goods_at_favorites(web_browser):
+    with allure.step('Запуск и настройка браузера'):
+        driver = MainPage(web_browser)
+        driver.cookies_button.click()
+        driver.close_button.click()
+
+    with allure.step('Добавляем товар в "Избранное"'):
+        driver.main_img.move_to_element()
+        element1 = driver.main_name.get_text()
+        driver.main_izbrannoe.click()
+
+    with allure.step('Переходим в "Избранное"'):
+        driver.action_button_favorites.click()
+        element2 = driver.favorite_name.get_text()
+        assert element1 == element2
+
+
+@allure.feature('Главная страница,Каталог')
+@allure.story('Фильтр "Сделано в Беларуси" работает корректно')
+def test_filter_made_in_belarus(web_browser):
+    with allure.step('Запуск и настройка браузера'):
+        driver = MainPage(web_browser)
+        driver.cookies_button.click()
+        driver.close_button.click()
+
+    with allure.step('Открываем Каталог'):
+        driver.catalog_button.click()
+
+    with allure.step('Выбираем раздел продукты '):
+        driver.katalog_prodycty.click()
+
+    with allure.step('Прокручиваем до фильтра "Сделано в Беларуси"'):
+        driver.scroll_down()
+
+    with allure.step(' Включаем фильтр "Сделано в Беларуси"'):
+        toggle=driver.made_in_belarus_toogle
+        toggle.click()
+        time.sleep(2)
+        aria_checked=driver.toggle_checkbox.get_attribute("aria-checked")
+        assert aria_checked == 'true'
+
+    with allure.step('Проверяем, что фильтр применен'):
+        bel_product = driver.made_in_belarus_product.click()
+        proizvoditel = driver.strana_product.get_text()
+        assert proizvoditel == 'Беларусь'
+
+@allure.feature("Главная страница")
+@allure.story("Футер, отображение и кликабельность элементов")
+def test_footers(web_browser):
+    with allure.step('Запуск и настройка браузера'):
+        driver = MainPage(web_browser)
+        driver.cookies_button.click()
+        driver.close_button.click()
+
+    with allure.step('Подготовка тестовых данных'):
+        elements = [
+            (driver.compania_button, 'Компания'),
+            (driver.servise_button, 'О сервисе'),
+            (driver.news_button, 'Новости'),
+            (driver.vacansii_button, 'Вакансии'),
+            (driver.sotrydnichestvo_button, 'Сотрудничество'),
+            (driver.dostavca_dlya_urlic_button, 'Доставка для юр.лиц'),
+            (driver.postavsikam_button, 'Поставщикам'),
+            (driver.reklamodatelam_button, 'Рекламодателям'),
+            (driver.stat_prodavcom_button, 'Стать продавцом'),
+            (driver.prodaza_avto_button, 'Продажа автомобилей'),
+            (driver.pokypatelam_button, 'Покупателям'),
+            (driver.polz_soglashenie_button, 'Пользовательское соглашение'),
+            (driver.vopros_otvet_button, 'Вопрос-ответ'),
+            (driver.dostavka_oplata_button, 'Доставка и оплата'),
+            (driver.catalog_button, 'Каталог'),
+            (driver.tovari_ot_emall_button, 'Товары от магазина Emall'),
+            (driver.punkti_vidachi_button, 'Пункты выдачи'),
+            (driver.soglashenie_o_creditax_button, 'Соглашение о кредитных ресурсах'),
+            (driver.nashi_druzia_button, 'Наши друзья'),
+            (driver.edostavka_button, 'Едоставка'),
+            (driver.evropochta_button, 'Европочта'),
+            (driver.evroopt_button, 'Евроопт'),
+            (driver.xit_button, 'Хит!'),
+            (driver.groshik_button, 'Грошык')
+        ]
+
+        skip_click_chek = ['Компания','Сотрудничество','Покупателям','Наши друзья']
+
+    with allure.step('Проверка элемента'):
+        for element, text_element in elements:
+            with allure.step(f'Проверка элемента {text_element} на отображение'):
+                check.is_true(element.is_visible(), f'Элемента {text_element} нет визуально на экарне')
+
+            if text_element not in skip_click_chek:
+                with allure.step(f'Проверка элемента {text_element} на кликабельность'):
+                    check.is_true(element.is_clickable(), f'Элемента {text_element} не кликабелен')
+
+
+@allure.feature('Главная страница')
+@allure.story('Переход на сраницу "Контакты" через футер')
+def test_kontakty(web_browser):
+    with allure.step('Запускаем и настройка браузер'):
+        driver = MainPage(web_browser)
+        driver.cookies_button.click()
+        driver.close_button.click()
+
+    with allure.step("Скроллим страницу вниз"):
+        driver._web_driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    with allure.step('Нажимаем на кнопку "Контакты" в футуре'):
+        driver.contacty_button.click()
+    with allure.step("Проверяем, что открылась страница Контакты"):
+        current_url = "https://emall.by/information/company/contacts"
+        # print("Текущий URL:", current_url)
+        assert current_url == "https://emall.by/information/company/contacts", \
+            f"Ожидали страницу Контакты, а открылась {current_url}"
+
+
+@allure.feature("Главная страница")
+@allure.story("Проверка всех ссылок футера и переход на них")
+def test_footer_links(web_browser):
+    with allure.step("Запуск браузера и закрытие баннеров"):
+        driver = MainPage(web_browser)
+        driver.cookies_button.click()
+        driver.close_button.click()
+
+    footer_links = [
+        (driver.servise_button, "https://emall.by/information/company/about-service"),
+        (driver.contacty_button, "https://emall.by/information/company/contacts"),
+        (driver.news_button, "https://emall.by/news"),
+        (driver.postavsikam_button, "https://emall.by/information/company/conditions-for-selecting-a-counterparty"),
+        (driver.reklamodatelam_button, "https://emall.by/information/company/advertisement"),
+        (driver.vopros_otvet_button, "https://emall.by/information/help"),
+        (driver.dostavka_oplata_button, "https://emall.by/information/help/132"),
+        (driver.tovari_ot_emall_button, "https://emall.by/shop/1"),
+        (driver.punkti_vidachi_button, "https://emall.by/map"),
+        (driver.vacansii_button, "https://jobs.e-dostavka.by/"),
+        (driver.dostavca_dlya_urlic_button, "https://business.emall.by/catalog/"),
+        (driver.stat_prodavcom_button, "https://seller.emall.by/"),
+        (driver.prodaza_avto_button, "https://emall.by/news/82"),
+        (driver.polz_soglashenie_button, "https://api-preprod.emall.by/storage/admin/files/yLqTfBKDsBPjdoPYehZxJushzuEyMifSngLFyMHH.pdf"),
+        (driver.soglashenie_o_creditax_button, "https://api-preprod.emall.by/storage/admin/files/UOEpXjJF8xBxksqpphDwn5HKKr6SizVhijbqxEww.pdf"),
+        (driver.edostavca_button, "https://edostavka.by/#modal-opened"),
+        (driver.evropochta_button, "https://evropochta.by/"),
+        (driver.evroopt_button, "https://evroopt.by/"),
+        (driver.xit_button, "https://hitdiscount.by/"),
+        (driver.groshik_button, "https://groshyk.by/"),
+        (driver.sviazatsa_snamy_button, "https://emall.by/?modal_id=feedback_modal&feedback_modal.props=%7B%7D"),
+    ]
+
+    for button, expected_url in footer_links:
+        with allure.step(f"Проверяем кнопку → {expected_url}"):
+            original_window = driver._web_driver.current_window_handle
+            windows_before = driver._web_driver.window_handles
+
+            button.click()  # кликаем по кнопке
+            driver.wait_page_loaded(timeout=120) # ждём загрузку страницы
+
+            windows_after = driver._web_driver.window_handles   # проверяем, открылась ли новая вкладка
+            if len(windows_after) > len(windows_before):
+                new_window = [w for w in windows_after if w not in windows_before][0]  # переключаемся на новую вкладку
+                driver._web_driver.switch_to.window(new_window)
+
+            current_url = driver.get_current_url() # берём текущий URL
+            assert current_url == expected_url, f"Ожидали {expected_url}, а получили {current_url}"
+
+            if len(windows_after) > len(windows_before): # если новая вкладка — закрываем её и возвращаемся обратно
+                driver._web_driver.close()
+                driver._web_driver.switch_to.window(original_window)
+            else:
+                driver.go_back()
+
+@allure.feature("Главная страница")
+@allure.story("Проверка количества отображаемых брендов с прокруткой")
+def test_brands_count(web_browser):
+    driver = MainPage(web_browser)
+
+    with allure.step("Настройка браузера"):
+        driver.cookies_button.click()
+        driver.close_button.click()
+
+    with allure.step("Нажимаем на кнопку открытия списка брендов"):
+        open_brands_button = driver.brands
+        open_brands_button.click()
+
+    with allure.step("Ожидаем видимость брендов"):
+        wait = WebDriverWait(web_browser, 20)
+        brands_elements = wait.until(
+            EC.visibility_of_all_elements_located(
+                (By.XPATH, '//a[contains(@class,"brands_brand_card")]')
+            )
+        )
+
+    with allure.step("Прокручиваем к каждому бренду"):
+        for brand in brands_elements:
+            ActionChains(web_browser).move_to_element(brand).perform()
+
+    with allure.step("Проверяем количество брендов"):
+        brands_count = len(brands_elements)
+        check.greater_equal(brands_count, 5)  # минимум 5 брендов
+        check.less_equal(brands_count, 1000)    # максимум 1000 брендов
